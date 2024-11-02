bea450619fae79d9336c448097999f0402676305
nurkeldi <mysiknomber1@gmail.com>
2024-11-02 13:12:01 +0600

    lesson 3

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+//dom
+const inputs = document.querySelectorAll('input')
+const selects = document.querySelectorAll('select')
+
+//dom EVENTS (onchange, oninput, onclick ...)
+
+inputs[0].addEventListener('input', calculate)
+selects[0].addEventListener('change', calculate)
+
+const currency = {
+    eur: 93.45,
+    usd: 85.80,
+    yu: 12.01
+    rub
+}
+
+function calculate() {
+    inputs[1].value = inputs[0].value * currency[selects[0].value]
+}
